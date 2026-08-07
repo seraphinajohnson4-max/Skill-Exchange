2e4b356bd01d2f916267d48f3e8220566202ae0e
seraphinajohnson4-max <seraphinajohnson4@gmail.com>
2026-08-07 14:38:37 +0100

    add supabase client config

---
+++ public/assets/js/supabase- client.js
@@ -0,0 +1,5 @@
+// Supabase connection setup
+const SUPABASE_URL = "https://jpjvmrzubqorygfoqdlg.supabase.co";
+const SUPABASE_KEY = "sb_publishable_kxlrSOI8r586Tv_vwgM6TA_SoL8Y-JN";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
